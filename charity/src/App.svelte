1f477e24a7613566da01a51fe3212b61a99315a5
unknown <ryannurman21@gmail.com>
2022-12-24 11:01:30 +0700

    ada perubahan  file

--- charity/src/App.svelte
+++ charity/src/App.svelte
@@ -1,30 +1,18 @@
 <script>
+	import Welcome from './Component/Welcome.svelte';
+	import Header from './Component/Header.svelte';
+	import CharityList from "./Component/CharityList.svelte";
+	
 	export let name;
 </script>
 
 <main>
-	<h1>hallo {name}!</h1>
-	<p>monggo <a href="https://svelte.dev/tutorial">monggo</a> mangga di ajar build Svelte apps.</p>
+	
+	<Header />
+	<CharityList />
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
